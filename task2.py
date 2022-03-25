fccd207bdff7e64782024a83cc6b9c6559467441
Tsovinar1986 <babakhanyan-tsovinar@rambler.ru>
2022-03-25 22:55:31 +0400

    github.com/Tsovinar1986/Lsaran.git

--- task2.py
+++ task2.py
@@ -5,38 +5,38 @@ class Namenum:
     def __init__(self,count):  
         self._count = count
     
-    # def get_count(self):
-    #       if sqrt(self._count) < 5:
-    #         return 'Yes'
-    #       else:
-    #         return 'No'   
+    def count(self):
+          if sqrt(self._count) < 5:
+            return 'Yes'
+          else:
+            return 'No'   
    
-    def set_numbersstr(self,name,count = 0):
-           self._name = name
-           self._count = count
-           if self._name == 'a' and self._name == 'j' and self._name == 's':
-            return self._count == 1
-           if self._name == 'b' and self._name == 'k' and self._name == 't':
-              return self._count == 2
-           if self._name == 'c' and self._name == 'l' and self._name == 'u':
-             return self._count == 3
-           if self._name == 'd' and self._name == 'm' and self._name == 'v':
-             return self._count ==4
-           if self._name == 'e' and self._name == 'n' and self._name == 'w':
-             return self._count == 5
-           if self._name == 'f' and self._name == 'o' and self._name == 'x':
-             return self._count == 6
-           if self._name == 'g' and self._name == 'p' and self._name == 'y':
-             return self._count == 7
-           if self._name == 'h' and self._name == 'q' and self._name == 'z':
-             return self._count == 8
-           if self._name == 'i' and self._name == 'r':
-             return self._count == 9
-           sum(map(int, self._count))
-name = input('Enter your name  ')
+    def numbersstr(self,name):
+           self._name1 = name
+           for i in self._name1:
+            if self._name1 == 'a' and self._name1 == 'j' and self._name1 == 's':
+                return  i == 1
+            if self._name1 == 'b' and self._name1 == 'k' and self._name1 == 't':
+                return i == 2
+            if self._name1 == 'c' and self._name1 == 'l' and self._name1 == 'u':
+               return i == 3
+            if self._name1 == 'd' and self._name1 == 'm' and self._name1 == 'v':
+                return  i == 4
+            if self._name1 == 'e' and self._name1 == 'n' and self._name1 == 'w':
+                return i == 5
+            if self._name1 == 'f' and self._name1 == 'o' and self._name1 == 'x':
+                return i == 6
+            if self._name1 == 'g' and self._name1 == 'p' and self._name1 == 'y':
+                return i == 7
+            if self._name1 == 'h' and self._name1 == 'q' and self._name1 == 'z':
+                return i == 8
+           if self._name1 == 'i' and self._name1 == 'r':
+             return i == 9
+           self._name1 += int(i)
+           self._count = self._name1
+           return self._count
+name = input('Enter your name please--> ')
 name1 = name.lower()
 y = Namenum(name1)
 print(y._count)
-print(y.set_numbersstr(name1))
-
-
+print(y.count())
