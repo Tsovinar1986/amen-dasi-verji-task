45fb624557267c669a22a22b4c8d2615decde53c
Tsovinar1986 <babakhanyan-tsovinar@rambler.ru>
2022-03-25 17:50:16 +0400

    github.com/Tsovinar1986/Lsaran.git

---
+++ task3.py
@@ -0,0 +1,27 @@
+from random import *
+
+class HarryPotter:
+    def __init__(self,barer,c):
+        self.barer = barer
+        self.c = c
+   
+    def  Vold(self):
+         while True:
+            self.barer == choice(barer) 
+            for i in self.barer:
+             if choice == i:
+                 self.c += 1
+                 print(self.c)
+                 continue
+             else:
+                 self.c == 0
+                 print(self.c)
+                 #continue
+    def count(self):
+        if self.c == 2:
+            return 'You win'
+        else:
+            return 'You lose'
+barer = ['Avada Kedavra', 'Crucio', 'Imperio']
+x = HarryPotter(barer,[])
+print(x.count())
