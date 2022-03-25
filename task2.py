3daba0638c41209a6961f26f483f49e8c75d8477
Tsovinar1986 <babakhanyan-tsovinar@rambler.ru>
2022-03-25 23:50:18 +0400

    github.com/Tsovinar1986/Lsaran.git

--- task2.py
+++ task2.py
@@ -14,23 +14,23 @@ class Namenum:
     def numbersstr(self,name):
            self._name1 = name
            for i in self._name1:
-            if self._name1 == 'a' and self._name1 == 'j' and self._name1 == 's':
+            if self._name1 == ('a' ,'j' , 's'):
                 return  i == 1
-            if self._name1 == 'b' and self._name1 == 'k' and self._name1 == 't':
+            if self._name1 == ('b','k' ,'t'):
                 return i == 2
-            if self._name1 == 'c' and self._name1 == 'l' and self._name1 == 'u':
+            if self._name1 == ('c','l','u'):
                return i == 3
-            if self._name1 == 'd' and self._name1 == 'm' and self._name1 == 'v':
+            if self._name1 == ('d','m','v'):
                 return  i == 4
-            if self._name1 == 'e' and self._name1 == 'n' and self._name1 == 'w':
+            if self._name1 == ('e', 'n', 'w'):
                 return i == 5
-            if self._name1 == 'f' and self._name1 == 'o' and self._name1 == 'x':
+            if self._name1 == ('f' , 'o' , 'x'):
                 return i == 6
-            if self._name1 == 'g' and self._name1 == 'p' and self._name1 == 'y':
+            if self._name1 == ('g','p' ,'y'):
                 return i == 7
-            if self._name1 == 'h' and self._name1 == 'q' and self._name1 == 'z':
+            if self._name1 == ('h', 'q','z'):
                 return i == 8
-           if self._name1 == 'i' and self._name1 == 'r':
+           if self._name1 == ('i', 'r'):
              return i == 9
            self._name1 += int(i)
            self._count = self._name1
