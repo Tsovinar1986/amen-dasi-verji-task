393e918d13f62875c2bcc9cd3c8ed041d500f25f
Tsovinar1986 <babakhanyan-tsovinar@rambler.ru>
2022-03-25 17:42:36 +0400

    https://github.com/Tsovinar1986/Lsaran.git

--- task2.py
+++ task2.py
@@ -1,40 +1,42 @@
 from math import sqrt
 
-class Namenum:
-    def __init__(self,name):
-        self.name  = name
-     
-        
-    def numbersstr(self,x):
-           self.x = x
-           self.name = self.name.lower()
-           if self.name == 'a' and self.name == 'j' and self.name == 's':
-            return  self.x == 1
-           if self.name == 'b' and self.name == 'k' and self.name == 't':
-            return self.x == 2
-           if self.name == 'c' and self.name == 'l' and self.name == 'u':
-             return self.x == 3
-           if self.name == 'd' and self.name == 'm' and self.name == 'v':
-             return self.x == 4
-           if self.name == 'e' and self.name == 'n' and self.name == 'w':
-              return self.x == 5
-           if self.name == 'f' and self.name == 'o' and self.name == 'x':
-             return self.x == 6
-           if self.name == 'g' and self.name == 'p' and self.name == 'y':
-             return  self.x == 7
-           if self.name == 'h' and self.name == 'q' and self.name == 'z':
-             return  self.x == 8
-           if self.name == 'i' and self.name == 'r':
-             return  self.x == 9      
-    def count(self):
-         if sqrt(self.name) < 5:
-                return 'Yes'
-         else:
-                return 'No'
-name = input('Your name please -- > ')
-y = Namenum(name)
-print(y.count())
-         
-        
 
+class Namenum:
+    def __init__(self,count):  
+        self._count = count
     
+    # def get_count(self):
+    #       if sqrt(self._count) < 5:
+    #         return 'Yes'
+    #       else:
+    #         return 'No'   
+   
+    def set_numbersstr(self,name,count = 0):
+           self._name = name
+           self._count = count
+           if self._name == 'a' and self._name == 'j' and self._name == 's':
+            return self._count == 1
+           if self._name == 'b' and self._name == 'k' and self._name == 't':
+              return self._count == 2
+           if self._name == 'c' and self._name == 'l' and self._name == 'u':
+             return self._count == 3
+           if self._name == 'd' and self._name == 'm' and self._name == 'v':
+             return self._count ==4
+           if self._name == 'e' and self._name == 'n' and self._name == 'w':
+             return self._count == 5
+           if self._name == 'f' and self._name == 'o' and self._name == 'x':
+             return self._count == 6
+           if self._name == 'g' and self._name == 'p' and self._name == 'y':
+             return self._count == 7
+           if self._name == 'h' and self._name == 'q' and self._name == 'z':
+             return self._count == 8
+           if self._name == 'i' and self._name == 'r':
+             return self._count == 9
+           sum(map(int, self._count))
+name = input('Enter your name  ')
+name1 = name.lower()
+y = Namenum(name1)
+print(y._count)
+print(y.set_numbersstr(name1))
+
+
